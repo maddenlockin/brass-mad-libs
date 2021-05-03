88329a13af089a32f37cbd2edbf8855508d1db81
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-05-03 12:09:04 -0700

    dimitra commits functions

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const verbTwo = document.getElementById('verb-two');
 const adjOne = document.getElementById('adj-one');
 const adjTwo = document.getElementById('adj-two');
 //img
-const img = document.getElementById('img');
+const img = document.getElementById('cover');
 //changes
 const nounOutputOne = document.getElementById('alonzo-one');
 const nounOutputTwo = document.getElementById('alonzo-two');
@@ -26,5 +26,31 @@ const adjOutputThree = document.getElementById('dj-one');
 //button
 const btn = document.getElementById('button');
 // initialize state
+btn.addEventListener('click', () =>{
+    const nounOneValue = nounOne.value;
+    const nounTwoValue = nounTwo.value;
+    const nounThreeValue = nounThree.value;
+    const verbOneValue = verbOne.value;
+    const verbTwoValue = verbTwo.value;
+    const adjOneValue = adjOne.value;
+    const adjTwoValue = adjTwo.value;
+
+    //getting words to change
+    nounOutputOne.textContent = nounOneValue;
+    nounOutputTwo.textContent = nounOneValue;
+    nounOutputThree.textContent = nounOneValue;
+    nounOutputFour.textContent = nounTwoValue;
+    nounOutputFive.textContent = nounThreeValue;
+    nounOutputSix.textContent = nounThreeValue;
+    nounOutputSeven.textContent = nounThreeValue;
+    nounOutputEight.textContent = nounThreeValue;
+    verbOutputOne.textContent = verbOneValue;
+    verbOutputTwo.textContent = verbTwoValue;
+    adjOutputOne.textContent = adjOneValue;
+    adjOutputTwo.textContent = adjTwoValue;
+    adjOutputThree.textContent = adjTwoValue;
+
+    img.src = "https://i.ytimg.com/vi/rnXUbePYbOk/maxresdefault.jpg";
+});
 
 // set event listeners to update state and DOM
