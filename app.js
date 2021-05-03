ffd94f342ad1d9c5e8ddd8b1147c66e61575d0bb
Alonzo A <68953196+Alonzo-Anderson@users.noreply.github.com>
2021-05-03 11:33:16 -0700

    alonzo commit

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 // import functions and grab DOM elements
-
+//inputs
+const nounOne = document.getElementById('noun-one');
+const nounTwo = document.getElementById('noun-two');
+const nounThree = document.getElementById('noun-three');
+const verbOne = document.getElementById('verb-one');
+const verbTwo = document.getElementById('verb-two');
+const adjOne = document.getElementById('adj-one');
+const adjTwo = document.getElementById('adj-two');
+//img
+const img = document.getElementById('img');
+//changes
+const nounOutputOne = document.getElementById('alonzo-one');
+const nounOutputTwo = document.getElementById('alonzo-two');
+const nounOutputThree = document.getElementById('alonzo-three');
+const nounOutputFour = document.getElementById('branden-one');
+const nounOutputFive = document.getElementById('madden-one');
+const nounOutputSix = document.getElementById('madden-two');
+const nounOutputSeven = document.getElementById('madden-three');
+const nounOutputEight = document.getElementById('madden-four');
+const verbOutputOne = document.getElementById('dimitra-one');
+const verbOutputTwo = document.getElementById('dimitra-two');
+const adjOutputOne = document.getElementById('dimitra-three');
+const adjOutputTwo = document.getElementById('dimitra-four');
+const adjOutputThree = document.getElementById('dj-one');
+//button
+const btn = document.getElementById('button');
 // initialize state
 
 // set event listeners to update state and DOM
